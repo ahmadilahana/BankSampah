2f43d633b9aa4dda0013b0465432c01b3f945128
ahmadilahana <ahmadilahana@gmail.com>
2021-06-15 12:52:48 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
 });
