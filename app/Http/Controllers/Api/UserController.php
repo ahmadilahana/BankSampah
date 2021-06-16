6356cb7d0a19d60fb117e4ff2ee1be1c59cc2198
ahmadilahana <ahmadilahana@gmail.com>
2021-06-17 06:25:15 +0700

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
     }
 
     public function login(){
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+        if(Auth::guard('api')->attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('nApp')->accessToken;
             return response()->json(['success' => $success], 200);
