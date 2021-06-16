3be7186a0fa895ebe29b77bb5112f66f7e9d6f9b
ahmadilahana <ahmadilahana@gmail.com>
2021-06-16 16:21:06 +0700

    update

--- database/migrations/2021_06_16_092036_ft_profile_table.php
+++ database/migrations/2021_06_16_092036_ft_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FtProfile extends Migration
+class FtProfileTable extends Migration
 {
     /**
      * Run the migrations.
